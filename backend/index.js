f8f10d66b209bb4625b178c3eb087a9466aa2be2
CrazyCoding330 <97684719+CrazyCoding330@users.noreply.github.com>
2022-11-30 08:21:19 -0500

    update backend

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,79 @@
 require('dotenv').config()
+const { v4: uuidv4 } = require("uuid");
+
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
 
+const AccessToken = require("twilio").jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const twilioClient = require('twilio')(accountSid, authToken);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-  app.get('/', (request, response) => {
-      response.send('Server is working')
-  })
+app.get('/', (request, response) => {
+  response.send('Server is working')
+})
 
-  app.post('/api/sendSMS', (request, response) => {
 
-    const { body, from, to} = request.body;
 
-    console.log(body, from, to);
-    try{
-        client.messages
-        .create({body: body, from: from, to: to})
-        .then(message => console.log(message.sid));
-        
-    } catch(err){
-        return response.status(500).json({msg: err.message})
+const PORT = 3001
+
+const findOrCreateRoom = async (roomName) => {
+  try {
+    // see if the room exists already. If it doesn't, this will throw
+    // error 20404.
+    await twilioClient.video.rooms(roomName).fetch();
+  } catch (error) {
+    // the room was not found, so create it
+    if (error.code == 20404) {
+      await twilioClient.video.rooms.create({
+        uniqueName: roomName,
+        type: "go",
+      });
+    } else {
+      // let other errors bubble up
+      throw error;
     }
-  })
+  }
+};
 
-  const PORT = 3001
-  app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-  })
+const getAccessToken = (roomName) => {
+  // create an access token
+  const token = new AccessToken(
+    accountSid,
+    authToken,
+    // generate a random unique identity for this participant
+    { identity: uuidv4() }
+  );
+  // create a video grant for this specific room
+  const videoGrant = new VideoGrant({
+    room: roomName,
+  });
+
+  // add the video grant
+  token.addGrant(videoGrant);
+  // serialize the token and return it
+  return token.toJwt();
+};
+app.post("/api/video/token", async (req, res) => {
+  // return 400 if the request has an empty body or no roomName
+
+  const roomName = "test";
+  // find or create a room with the given roomName
+  findOrCreateRoom(roomName);
+  // generate an Access Token for a participant in this room
+  const token = getAccessToken(roomName);
+  res.send({
+    token: token,
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
